b733bb7ed770af24cd70149e1ba36ddf7b4f7dac
Victor Tran <vicr12345@gmail.com>
2023-09-10 17:34:26 +1000

    Update translations

--- application/translations/en_US.ts
+++ application/translations/en_US.ts
@@ -4,27 +4,17 @@
 <context>
     <name>AddLocationPopover</name>
     <message>
-        <source>Form</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../addlocationpopover.ui" line="+35"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_addlocationpopover.h" line="+147"/>
         <source>Add Location</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+7"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_addlocationpopover.h" line="+1"/>
         <source>Where do you want to see the weather?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+26"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_addlocationpopover.h" line="+1"/>
         <source>Search</source>
         <translation type="unfinished"></translation>
     </message>
@@ -32,68 +22,43 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <source>MainWindow</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../mainwindow.ui" line="+116"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="+175"/>
         <source>File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+15"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="-13"/>
         <source>Exit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+3"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="+2"/>
         <source>Ctrl+Q</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+9"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="+2"/>
         <source>Close Tab</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+3"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="+2"/>
         <source>Ctrl+W</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+9"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="+2"/>
         <source>Add City</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+3"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="+2"/>
         <source>Ctrl+T</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="+111"/>
         <source>My Location</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>TemperatureTimeWidget</name>
-    <message>
-        <source>Form</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -133,20 +98,12 @@
 <context>
     <name>WeatherPane</name>
     <message>
-        <source>Form</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../weatherpane.ui" line="+138"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_weatherpane.h" line="+141"/>
         <source>Open System Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+7"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_weatherpane.h" line="+1"/>
         <source>Grant Permission</source>
         <translation type="unfinished"></translation>
     </message>
@@ -166,12 +123,7 @@
 <context>
     <name>WeatherWidget</name>
     <message>
-        <source>Form</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../weather/weatherwidget.ui" line="+146"/>
-        <location/>
         <source>Hourly Weather</source>
         <translation type="unfinished"></translation>
     </message>
--- application/translations/he.ts
+++ application/translations/he.ts
@@ -4,27 +4,17 @@
 <context>
     <name>AddLocationPopover</name>
     <message>
-        <source>Form</source>
-        <translation></translation>
-    </message>
-    <message>
         <location filename="../addlocationpopover.ui" line="+35"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_addlocationpopover.h" line="+147"/>
         <source>Add Location</source>
         <translation>הוספת מקום</translation>
     </message>
     <message>
         <location line="+7"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_addlocationpopover.h" line="+1"/>
         <source>Where do you want to see the weather?</source>
         <translation>איפה ברצונך לדעת את מזג האוויר?</translation>
     </message>
     <message>
         <location line="+26"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_addlocationpopover.h" line="+1"/>
         <source>Search</source>
         <translation>חיפוש</translation>
     </message>
@@ -32,55 +22,37 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <source>MainWindow</source>
-        <translation></translation>
-    </message>
-    <message>
         <location filename="../mainwindow.ui" line="+116"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="+175"/>
         <source>File</source>
         <translation>קובץ</translation>
     </message>
     <message>
         <location line="+15"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="-13"/>
         <source>Exit</source>
         <translation>יציאה</translation>
     </message>
     <message>
         <location line="+3"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="+2"/>
         <source>Ctrl+Q</source>
         <translation>Ctrl+Q</translation>
     </message>
     <message>
         <location line="+9"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="+2"/>
         <source>Close Tab</source>
         <translation>סגור כרטיסייה</translation>
     </message>
     <message>
         <location line="+3"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="+2"/>
         <source>Ctrl+W</source>
         <translation>Ctrl+W</translation>
     </message>
     <message>
         <location line="+9"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="+2"/>
         <source>Add City</source>
         <translation>הוסף עיר</translation>
     </message>
     <message>
         <location line="+3"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="+2"/>
         <source>Ctrl+T</source>
         <translation>Ctrl+T</translation>
     </message>
@@ -92,10 +64,6 @@
 </context>
 <context>
     <name>TemperatureTimeWidget</name>
-    <message>
-        <source>Form</source>
-        <translation></translation>
-    </message>
     <message>
         <source>%1 mm</source>
         <translation type="vanished">%1 מ&quot;מ</translation>
@@ -137,20 +105,12 @@
 <context>
     <name>WeatherPane</name>
     <message>
-        <source>Form</source>
-        <translation></translation>
-    </message>
-    <message>
         <location filename="../weatherpane.ui" line="+138"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_weatherpane.h" line="+141"/>
         <source>Open System Settings</source>
         <translation>פתח הגדרות מערכת</translation>
     </message>
     <message>
         <location line="+7"/>
-        <location/>
-        <location filename="../../../build-theTroposphere-Desktop_ARM-Debug/application/thetroposphere_autogen/include/ui_weatherpane.h" line="+1"/>
         <source>Grant Permission</source>
         <translation>תן הרשאה</translation>
     </message>
@@ -170,12 +130,7 @@
 <context>
     <name>WeatherWidget</name>
     <message>
-        <source>Form</source>
-        <translation></translation>
-    </message>
-    <message>
         <location filename="../weather/weatherwidget.ui" line="+146"/>
-        <location/>
         <source>Hourly Weather</source>
         <translation>מזג אוויר לפי שעה</translation>
     </message>
