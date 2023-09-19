2569d055b40005e0ee9ad6c118eaf3e911e6db1e
Parlance <parlance@vicr123.com>
2023-09-19 09:31:00 +0000

    Update Translations

---
+++ application/translations/vi.ts
@@ -0,0 +1,172 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<TS version="2.1" language="vi">
+  <context>
+    <name>AddLocationPopover</name>
+    <message>
+      <translation></translation>
+      <location filename="../addlocationpopover.ui" line="+35" />
+      <location filename="../../../build-theTroposphere-Android_Qt_6_5_2_aarch64_Clang_arm64_v8a-Debug/application/thetroposphere_autogen/include/ui_addlocationpopover.h" line="+147" />
+      <source>Add Location</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../../../build-theTroposphere-Android_Qt_6_5_2_aarch64_Clang_arm64_v8a-Debug/application/thetroposphere_autogen/include/ui_addlocationpopover.h" line="+1" />
+      <source>Where do you want to see the weather?</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../../../build-theTroposphere-Android_Qt_6_5_2_aarch64_Clang_arm64_v8a-Debug/application/thetroposphere_autogen/include/ui_addlocationpopover.h" line="+1" />
+      <source>Search</source>
+    </message>
+  </context>
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.ui" line="+116" />
+      <location filename="../../../build-theTroposphere-Android_Qt_6_5_2_aarch64_Clang_arm64_v8a-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="+183" />
+      <source>File</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../../../build-theTroposphere-Android_Qt_6_5_2_aarch64_Clang_arm64_v8a-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="-17" />
+      <source>Exit</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../../../build-theTroposphere-Android_Qt_6_5_2_aarch64_Clang_arm64_v8a-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="+2" />
+      <source>Ctrl+Q</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../../../build-theTroposphere-Android_Qt_6_5_2_aarch64_Clang_arm64_v8a-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="+2" />
+      <source>Close Tab</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../../../build-theTroposphere-Android_Qt_6_5_2_aarch64_Clang_arm64_v8a-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="+2" />
+      <source>Ctrl+W</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../../../build-theTroposphere-Android_Qt_6_5_2_aarch64_Clang_arm64_v8a-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="+2" />
+      <source>Add City</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../../../build-theTroposphere-Android_Qt_6_5_2_aarch64_Clang_arm64_v8a-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="+2" />
+      <source>Ctrl+T</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../../../build-theTroposphere-Android_Qt_6_5_2_aarch64_Clang_arm64_v8a-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="+2" />
+      <source>Settings</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../../../build-theTroposphere-Android_Qt_6_5_2_aarch64_Clang_arm64_v8a-Debug/application/thetroposphere_autogen/include/ui_mainwindow.h" line="+2" />
+      <source>Ctrl+,</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="+113" />
+      <source>My Location</source>
+    </message>
+  </context>
+  <context>
+    <name>TemperatureUnitSettingsPane</name>
+    <message>
+      <translation></translation>
+      <location filename="../settings/temperatureunitsettingspane.ui" line="+20" />
+      <source>Auto</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>Celsius</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>Fahrenheit</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../settings/temperatureunitsettingspane.cpp" line="+32" />
+      <source>Temperature</source>
+    </message>
+  </context>
+  <context>
+    <name>TroposphereHelper</name>
+    <message>
+      <translation></translation>
+      <location filename="../tropospherehelper.cpp" line="+23" />
+      <source>%1 ℃</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>%1 km/h</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>%1 mph</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>%1 mm</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>%1 in</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>%1 ℉</source>
+    </message>
+  </context>
+  <context>
+    <name>WeatherPane</name>
+    <message>
+      <translation></translation>
+      <location filename="../weatherpane.ui" line="+138" />
+      <location filename="../../../build-theTroposphere-Android_Qt_6_5_2_aarch64_Clang_arm64_v8a-Debug/application/thetroposphere_autogen/include/ui_weatherpane.h" line="+141" />
+      <source>Open System Settings</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../../../build-theTroposphere-Android_Qt_6_5_2_aarch64_Clang_arm64_v8a-Debug/application/thetroposphere_autogen/include/ui_weatherpane.h" line="+1" />
+      <source>Grant Permission</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../weatherpane.cpp" line="+65" />
+      <source>Permissions Required</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>theTroposphere needs to be able to access your location in order to show you the weather at your location.</source>
+    </message>
+  </context>
+  <context>
+    <name>WeatherWidget</name>
+    <message>
+      <translation></translation>
+      <location filename="../weather/weatherwidget.ui" line="+146" />
+      <source>Hourly Weather</source>
+    </message>
+  </context>
+  <context>
+    <name>main</name>
+    <message>
+      <translation></translation>
+      <location filename="../main.cpp" line="+18" />
+      <source>Weather</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>Weather data from MET Norway</source>
+    </message>
+    <message>
+      <translation></translation>
+      <source>Units</source>
+    </message>
+  </context>
+</TS>
